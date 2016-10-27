9d8ec2bd1395036b446deed780e5efca67f3dbe2
Vt97 <vincent.taylor97@gmail.com>
2016-10-27 01:15:28 -0400

    Update globe.js

--- globe.js
+++ globe.js
@@ -105,7 +105,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + '/WebGL_globe/world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir + '/MSGlobe/world.jpg');
 
     material = new THREE.ShaderMaterial({
 
